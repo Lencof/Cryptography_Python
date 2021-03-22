0388d764780d413abf26b818f1cc2af70e7b49a2
Lencof <71154457+Lencof@users.noreply.github.com>
2021-03-22 09:23:23 +0300

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -1,12 +1,12 @@
 # __Author__ __Lencof__
 # Crypto.py
 
->>> from cryptography.fernet import Fernet
->>> # Put this somewhere safe!
->>> key = Fernet.generate_key()
->>> f = Fernet(key)
->>> token = f.encrypt(b"A really secret message. Not for prying eyes.")
->>> token
+from cryptography.fernet import Fernet
+# Put this somewhere safe!
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(b"A really secret message. Not for prying eyes.")
+token
 b'...'
->>> f.decrypt(token)
+f.decrypt(token)
 b'A really secret message. Not for prying eyes.'
